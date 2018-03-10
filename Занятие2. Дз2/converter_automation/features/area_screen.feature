0a64eff390a1e27be1ae71ae68e7f32aa526aa9c
Shajdullina Elyana <lelechka_29@mail.ru>
2018-03-10 13:23:57 +0400

    Занятие2.ДЗ2

---
+++ Занятие2. Дз2/converter_automation/features/area_screen.feature
@@ -0,0 +1,79 @@
+@area
+Feature: User is able to convert area units
+
+  Background:
+    Given I click on Got it button
+
+    #Scenario: User to able to dismiss help
+    #Given I land on help
+    #When I click on Got it button
+    #Then I land on Area screen
+
+
+  Scenario: User is able to swap values
+    #ошибка для проверки теста From Header
+      Given I see "Sq KilometrA" in From Header
+
+      And I see "Sq Metre" in To header
+      When I click on Swap button
+      Then I see "Sq Metre" in From Header
+      And I see "Sq Kilometre" in To header
+
+
+  Scenario: User is able to swap values
+    Given I see "Sq Kilometre" in From Header
+    And I see "Sq Metre" in To header
+    When I click on Swap button
+    Then I see "Sq Metre" in From Header
+    #ошибка для проверки теста To Header
+    And I see "Sq KilometrA" in To header
+
+
+  Scenario Outline: User is able to convert default units
+       Given I click on Clear button
+        When I enter "<target>" to From field
+        Then I get "<result>" in To field
+
+        Examples:
+        |target|result|
+        |1     |1000000|
+      #  |2     |2000000|
+      #  |3     |3000000|
+
+
+Scenario: User is able to use soft keyboard to enter values
+  Given I click on Clear button
+  When I click on From field
+  And I press "47" on soft keyboard
+    #для проверки теста
+  Then I get "47000000" in To field
+
+
+  Scenario:
+      When I select "Hectare" from left column
+      Then I see "Hectare" in From header
+      And I get "10000" in To field
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Занятие2. Дз2/converter_automation/features/step_definitions/area screen steps.rb
@@ -0,0 +1,62 @@
+Given(/^I land on help$/) do
+  text("Help")
+end
+
+When(/^I click on Got it button$/) do
+  find_element(id:"button1").click
+end
+
+Then(/^I land on Area screen$/) do
+ text("Area")
+end
+
+When(/^I click on Swap button$/) do
+   find_element(id:"fab").click
+end
+
+Then(/^I see "([^"]*)" in From Header$/) do |value|
+  actual_value = find_element(id:"header_text_unit_from").text
+  if actual_value != value
+    fail ("Expected value in FROM HEADER is #{value}, but actual value was #{actual_value}")
+  end
+end
+
+And(/^I see "([^"]*)" in To header$/) do |value|
+  actual_value = find_element(id:"header_text_unit_to").text
+  #puts("Expected value is " + value)
+  #puts("Actual value is " + actual_value)
+     if actual_value != value
+  fail ("Expected value in TO HEADER is #{value}, but actual value was #{actual_value}")
+   end
+  end
+
+And(/^I click on Clear button$/) do
+  find_element(id:"menu_clear").click
+end
+
+When(/^I enter "([^"]*)" to From field$/) do |value|
+  find_element(id:"header_value_from").send_keys(value)
+end
+
+Then(/^I get "([^"]*)" in To field$/) do |value|
+  actual_value = find_element(id:"header_value_to").text
+  if actual_value != value
+    fail ("Expected value is #{value}, but actual value was #{actual_value}")
+  end
+end
+
+When(/^I click on From field$/) do
+  find_element(id:"header_value_from").click
+end
+
+And(/^I press "([^"]*)" on soft keyboard$/) do |value|
+  digits = value.split("") #массив с данными
+  digits.each do |key|
+  digit = Integer(key)
+  press_keycode 7 + digit
+end
+  end
+
+When(/^I select "([^"]*)" from left column$/) do |value|
+  find_element(id: "radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']").click
+ end
